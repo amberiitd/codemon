cd113202c3c4d1ba81f60e6c52fcf4c5862cd2c2
amberiitd <amberiitd15@gmail.com>
2024-01-18 05:57:15 +0400

    feat: websocket init

--- frontend/src/api/execute.js
+++ frontend/src/api/execute.js
@@ -9,33 +9,6 @@ import { API } from "aws-amplify";
 import { languages } from "../constants/general";
 
 export const execute = (script, stdin, language, output) => {
-	// return new Promise((resolve, reject) => {
-	// 	// resolve({
-	// 	// 	output: output,
-	// 	// 	statusCode: 200,
-	// 	// 	memory: "",
-	// 	// 	cpuTime: "",
-	// 	// });
-
-	// 	reject({
-	// 	  error: "Error",
-	// 	  statusCode: 500
-	// 	})
-	// }).catch((error) => {return error;});
-	//  fetch(`${process.env.REACT_APP_JBOOT_BASE_URL}/execute`, {
-	// 	method: "POST",
-	//   headers: {
-	//     "Content-Type": "application/json"
-	//   },
-	// body: JSON.stringify({
-	// 	clientId: process.env.REACT_APP_JBOOT_CLIENT_ID,
-	// 	clientSecret: process.env.REACT_APP_JBOOT_CLIENT_SECRET,
-	// 	script,
-	// 	stdin,
-	// 	language,
-	// 	versionIndex: languages[language].version,
-	// }),
-	// })
 	return API.post("base_url", "/execute", {
 		body: { script, stdin, language: languages[language].codeName, versionIndex: languages[language].version },
 	})
@@ -48,6 +21,17 @@ export const execute = (script, stdin, language, output) => {
 				error: error.message,
 				statusCode: 500,
 			};
+		});
+};
+
+export const getToken = () => {
+	return API.get("base_url", "/token")
+		.then((data) => {
+			return data.body;
+		})
+		.catch((error) => {
+			console.error(error);
+			return null;
 		});
 };
 
--- frontend/src/components/BottomPanel.jsx
+++ frontend/src/components/BottomPanel.jsx
@@ -1,22 +1,17 @@
 import { useTheme } from "@emotion/react";
 import { Editor } from "@monaco-editor/react";
-import {
-	Box,
-	Button,
-	ButtonGroup,
-	CircularProgress,
-	Typography,
-} from "@mui/material";
+import { Box, Button, ButtonGroup, CircularProgress, Typography } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
 import { useTestContext } from "../pages/test";
-import { useContext, useEffect, useMemo } from "react";
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { AppContext } from "../contexts/app";
 import usePageQuery from "../hooks/usePageQuery";
-import { execute } from "../api/execute";
+import { execute, getToken } from "../api/execute";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import { verifyResult } from "../util/general";
 import TestRunTab from "./TestRunTab";
+import { languages } from "../constants/general";
 
 const BottomPanel = () => {
 	const { problemId } = useParams();
@@ -41,6 +36,63 @@ const BottomPanel = () => {
 		}
 	}, [runQuery.data]);
 
+	const socketClient = useMemo(
+		() =>
+			window.webstomp.over(new window.SockJS(`${process.env.REACT_APP_JBOOT_BASE_URL}/stomp`), {
+				heartbeat: false,
+				debug: true,
+			}),
+		[]
+	);
+	const [apiToken, setApiToken] = useState();
+	const sendRunRequest = useCallback(() => {
+		let data = JSON.stringify({
+			script: problem.editor[problem.language],
+			language: languages[problem.language].codeName,
+			versionIndex: languages[problem.language].version,
+		});
+
+		if (!socketClient || socketClient.status === "CONNECTED") return;
+
+		socketClient.send("/app/execute-ws-api-token", data, {
+			message_type: "execute",
+			token: apiToken,
+		});
+	}, [socketClient, problem, apiToken]);
+
+	useEffect(() => {
+		if (!socketClient || socketClient.status === "CONNECTED") return;
+		const onWsConnection = () => {
+			socketClient.subscribe("/user/queue/execute-i", (message) => {
+				let msgId = message.headers["message-id"];
+				let msgSeq = parseInt(msgId.substring(msgId.lastIndexOf("-") + 1));
+				let statusCode = parseInt(message.headers.statusCode);
+				console.log("message", message);
+			});
+		};
+		const onWsConnectionFailed = (e) => {
+			console.log("connection failed");
+			console.log(e);
+		};
+		socketClient.connect({}, onWsConnection, onWsConnectionFailed);
+		return () => {
+			if (socketClient.status === "CONNECTED") socketClient.disconnect();
+		};
+	}, [socketClient]);
+
+	useEffect(() => {
+		if (!apiToken) {
+			getToken()
+				.then((data) => {
+					console.log(data);
+					setApiToken(data);
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		}
+	}, []);
+
 	return (
 		<Box height={"30%"} overflow={"auto"} paddingBottom={0} paddingTop={2}>
 			<Box
@@ -56,12 +108,13 @@ const BottomPanel = () => {
 						variant="outlined"
 						size="small"
 						onClick={() => {
-							runQuery.trigger(
-								problem.editor[problem.language],
-								problem.testCases[0].input,
-								problem.language,
-								problem.testCases[0].output
-							);
+							// runQuery.trigger(
+							// 	problem.editor[problem.language],
+							// 	problem.testCases[0].input,
+							// 	problem.language,
+							// 	problem.testCases[0].output
+							// );
+							sendRunRequest();
 						}}
 					>
 						Run
